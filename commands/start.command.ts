4b53cd9587e1bd7b586c3b2f77dec0e53a9d5bda
worthant <b_dvorkin@niuitmo.ru>
2023-07-31 01:20:36 +0300

    start command works

--- commands/start.command.ts
+++ commands/start.command.ts
@@ -1,30 +1,59 @@
 import { Telegraf } from 'telegraf';
-import { OpenAIApi } from 'openai';
 import { IBotContext } from '../src/context/context.interface';
 import { Command } from './command.class';
+import axios from 'axios';
 
 export class StartCommand extends Command {
-    constructor(bot: Telegraf<IBotContext>) {
+    private readonly rapidAPIKey: string;
+
+    constructor(bot: Telegraf<IBotContext>, rapidAPIKey: string) {
         super(bot);
+        this.rapidAPIKey = rapidAPIKey;
     }
-    
-    async handle(openai: OpenAIApi): Promise<void> {
+
+    handle(): void {
         this.bot.start((ctx) => {
             ctx.reply('Please type a message');
         });
 
         this.bot.on('text', async (ctx) => {
             const message = ctx.message.text;
-            const chatCompletion = await openai.createChatCompletion({
-                model: "gpt-3.5-turbo",
-                messages: [{role: "user", content: message}],
-            });
 
-            if (chatCompletion.data.choices[0]?.message?.content) {
-                const reply = chatCompletion.data.choices[0].message.content;
-                ctx.reply(reply);
-            } else {
-                ctx.reply("Sorry, I couldn't generate a response.");
+            const options = {
+                method: 'POST',
+                url: 'https://open-ai21.p.rapidapi.com/conversationllama',
+                headers: {
+                    'content-type': 'application/json',
+                    'X-RapidAPI-Key': this.rapidAPIKey,
+                    'X-RapidAPI-Host': 'open-ai21.p.rapidapi.com'
+                },
+                data: {
+                    messages: [
+                        {
+                            role: 'user',
+                            content: message
+                        }
+                    ],
+                    web_access: false
+                }
+            };
+
+            try {
+                const response = await axios.request(options);
+
+                if (response.data && Array.isArray(response.data.messages) && response.data.messages.length > 0) {
+                    const reply = response.data.messages.pop()?.content;
+                    if (reply) {
+                        ctx.reply(reply);
+                    } else {
+                        ctx.reply("Sorry, the AI's response was empty.");
+                    }
+                } else {
+                    ctx.reply("Sorry, I couldn't generate a response.");
+                }
+            } catch (error) {
+                console.error(error);
+                ctx.reply("Sorry, an error occurred while generating a response.");
             }
         });
     }
--- src/app.ts
+++ src/app.ts
@@ -1,73 +1,29 @@
-// import { Telegraf } from "telegraf";
-// import { IConfigService } from "./config/config.interface";
-// import { ConfigService } from "./config/config.service";
-// import { IBotContext } from "./context/context.interface";
-// import { Command } from "../commands/command.class";
-// import { StartCommand } from "../commands/start.command";
-// import LocalSession from "telegraf-session-local";
-// import { OpenAIApi, Configuration } from 'openai';
+import { Telegraf } from "telegraf";
+import { IConfigService } from "./config/config.interface";
+import { ConfigService } from "./config/config.service";
+import { IBotContext } from "./context/context.interface";
+import { Command } from "../commands/command.class";
+import { StartCommand } from "../commands/start.command";
+import LocalSession from "telegraf-session-local";
 
-import { Telegraf } from 'telegraf';
-import { Configuration, OpenAIApi } from 'openai';
+class Bot {
+    private bot: Telegraf<IBotContext>;
+    private commands: Command[] = [];
 
-// Replace 'your-telegram-bot-token' and 'your-openai-api-key' with your actual tokens
-const bot = new Telegraf('6287109942:AAGqv_8Y9am_vOd7QAq49mdyCLfn6U2HWM0');
-//const configuration = new Configuration({ apiKey: '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' });
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-bot.start((ctx) => ctx.reply('Please type a message'));
-
-bot.on('text', async (ctx) => {
-    const chatCompletion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: ctx.message.text }],
-    });
-
-    if (chatCompletion.data.choices[0]?.message?.content) {
-        const reply = chatCompletion.data.choices[0].message.content;
-        ctx.reply(reply);
-    } else {
-        ctx.reply("Sorry, I couldn't generate a response.");
+    constructor(private readonly configService: IConfigService) {
+        this.bot = new Telegraf<IBotContext>(this.configService.get('TELEGRAM_BOT_TOKEN'));
+        this.bot.use(new LocalSession({ database: "sessions.json" }).middleware());
     }
-});
 
-bot.launch();
+    init() {
+        const rapidAPIKey = this.configService.get('RAPIDAPI_KEY');
+        this.commands = [new StartCommand(this.bot, rapidAPIKey)];
+        for (const command of this.commands) {
+            command.handle();
+        }
+        this.bot.launch();
+    }
+}
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-// class Bot {
-//     private bot: Telegraf<IBotContext>;
-//     private commands: Command[] = [];
-//     private openai: OpenAIApi;
-
-//     constructor(private readonly configService: IConfigService) {
-//         this.bot = new Telegraf<IBotContext>(this.configService.get('TELEGRAM_BOT_TOKEN'));
-//         this.bot.use(new LocalSession({ database: "sessions.json" }).middleware());
-
-//         const configuration = new Configuration({
-//             apiKey: this.configService.get('OPENAI_API_KEY'),
-//         });
-//         this.openai = new OpenAIApi(configuration);
-//     }
-
-//     async init() {
-//         const configuration = new Configuration({
-//             apiKey: this.configService.get('OPENAI_API_KEY'),
-//         });
-//         const openai = new OpenAIApi(configuration);
-
-//         this.commands = [new StartCommand(this.bot)];
-//         for (const command of this.commands) {
-//             await command.handle(openai);
-//         }
-//         this.bot.launch();
-//     }
-// }
-
-// const bot = new Bot(new ConfigService());
-// bot.init();
-
+const bot = new Bot(new ConfigService());
+bot.init();
